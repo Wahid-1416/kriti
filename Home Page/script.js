7c3178f7342ee7c5f5e7298ae358a63f900ec773
Wahid-1416 <147654913+Wahid-1416@users.noreply.github.com>
2024-02-05 16:02:42 +0530

    Sulabh

--- Home Page/script.js
+++ Home Page/script.js
@@ -1,5 +1,25 @@
-var var1=Document.quarrySelector("#project1");
+function swipper(){
 
-var1.addEventListner("onClick",{
+    var swiper = new Swiper('.swiper', {
+        slidesPerView: 3,
+        direction: getDirection(),
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        on: {
+          resize: function () {
+            swiper.changeDirection(getDirection());
+          },
+        },
+      });
     
-})
+      function getDirection() {
+        var windowWidth = window.innerWidth;
+        var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+    
+        return direction;
+      }
+}
+
+swipper()
