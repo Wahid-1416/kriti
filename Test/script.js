9550a9c0965eaea2f8e0c0bf041e54a872769633
Wahid-1416 <147654913+Wahid-1416@users.noreply.github.com>
2024-02-09 03:55:14 +0530

    profile page

--- Test/script.js
+++ Test/script.js
@@ -1,35 +1,11 @@
-const stars = document.querySelectorAll('.star');
-const userRating = document.querySelector('.user-rating');
-
-let selectedRating = 0;
-
-stars.forEach((star) => {
-  star.addEventListener('mouseover', () => {
-    const rating = parseInt(star.getAttribute('data-rating'));
-    highlightStars(rating);
-  });
-
-  star.addEventListener('mouseout', () => {
-    highlightStars(selectedRating);
-  });
-
-  star.addEventListener('click', () => {
-    selectedRating = parseInt(star.getAttribute('data-rating'));
-    displayUserRating(selectedRating);
-  });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
-
-function highlightStars(rating) {
-  stars.forEach((star) => {
-    const starRating = parseInt(star.getAttribute('data-rating'));
-    if (starRating <= rating) {
-      star.src = './star.svg';
-    } else {
-      star.src = './unstar.svg';
-    }
-  });
-}
-
-function displayUserRating(rating) {
-  userRating.textContent = `You rated this ${rating} stars.`;
-}
